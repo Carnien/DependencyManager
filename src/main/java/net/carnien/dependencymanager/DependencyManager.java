2b1590534c5ae10e056fc0a3a817757c7a8e7a22
Carnivohra <carnivohra@gmail.com>
2024-04-18 03:29:32 +0200

    Rewrite structure

--- src/main/java/net/carnien/dependencymanager/DependencyManager.java
+++ src/main/java/net/carnien/dependencymanager/DependencyManager.java
@@ -6,10 +6,7 @@ import org.bukkit.plugin.InvalidPluginException;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
@@ -31,49 +28,15 @@ public final class DependencyManager {
         dependencies.remove(dependency);
     }
 
-    public Dependency[] getAll() {
+    public Dependency[] get() {
         return dependencies.toArray(new Dependency[]{});
     }
 
-    public boolean check() {
-        Dependency[] missingDependencies = getMissingDependencies();
+    private void downloadMissingDependencies() {
+        final Dependency[] missingDependencies = getMissingDependencies();
 
-        if (missingDependencies.length == 0) return true;
+        if (missingDependencies.length == 0) return;
 
-        downloadDependencies(missingDependencies);
-        missingDependencies = getMissingDependencies();
-        final Logger logger = Bukkit.getLogger();
-
-        if (missingDependencies.length != 0) {
-            logger.warning("Missing dependencies. Please download them first!");
-            return false;
-        }
-
-        if (isReloadRequired()) {
-            logger.warning("Please restart or reload the server to enable all dependencies!");
-            return false;
-        }
-
-        return true;
-    }
-
-    public Dependency[] getMissingDependencies() {
-        final List<Dependency> missingDependencies = new ArrayList<>();
-
-        for (Dependency dependency : dependencies) {
-            final String name = dependency.getName();
-            final PluginManager pluginManager = Bukkit.getPluginManager();
-            Plugin plugin = pluginManager.getPlugin(name);
-
-            if (plugin != null) continue;
-
-            missingDependencies.add(dependency);
-        }
-
-        return missingDependencies.toArray(new Dependency[]{});
-    }
-
-    public void downloadDependencies(Dependency[] dependencies) {
         final Logger logger = Bukkit.getLogger();
         logger.info("Downloading missing dependencies ...");
 
@@ -95,8 +58,8 @@ public final class DependencyManager {
             try {
                 pluginManager.loadPlugin(file);
             } catch (InvalidPluginException | InvalidDescriptionException exception) {
-                logger.warning("An error accorded while enabling plugin '" + name + "'.");
-                throw new RuntimeException();
+                logger.warning("An error accorded while loading plugin '" + name + "'.");
+                return;
             }
 
             logger.info("Plugin '" + name + "' has been loaded successfully.");
@@ -116,6 +79,22 @@ public final class DependencyManager {
         }
 
         return false;
+    }
+
+    private Dependency[] getMissingDependencies() {
+        final List<Dependency> missingDependencies = new ArrayList<>();
+
+        for (Dependency dependency : dependencies) {
+            final String name = dependency.getName();
+            final PluginManager pluginManager = Bukkit.getPluginManager();
+            Plugin plugin = pluginManager.getPlugin(name);
+
+            if (plugin != null) continue;
+
+            missingDependencies.add(dependency);
+        }
+
+        return missingDependencies.toArray(new Dependency[]{});
     }
 
     private boolean download(Dependency dependency) {
